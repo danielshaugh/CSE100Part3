9893b239706bd772979fa24bf07e37b0151cfeac
danielshaugh@gmail.com <danielshaugh@gmail.com>
2019-03-14 19:38:50 -0700

    test

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -52,7 +52,7 @@ Node* Graph::insertNode(int type, string value) {
   Node* temp = new Node(type, value);
   auto output = this->nodes.insert(make_pair(value,temp));
   // return output value
-  temp = *(output.first->second);
+  *temp = (output.first->second);
   return temp;
 }
 
